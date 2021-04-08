dccf006459f803f6e79ddc561fae0b2613f24878
Savoury SnaX <savoury.snax@googlemail.com>
2021-04-08 14:05:42 +0100

    Check local type and variable definition recovery

--- HumphreyCompiler/src/FrontEnd/CompilerMessages.cs
+++ HumphreyCompiler/src/FrontEnd/CompilerMessages.cs
@@ -20,6 +20,7 @@ namespace Humphrey.FrontEnd
         Error_DuplicateSymbol = Error | ParseError | 0x08,
         Error_MustBeExpression = Error | ParseError | 0x09,
         Error_ExpectedIdentifier = Error | ParseError | 0x0A,
+        Error_ExpectedType = Error | ParseError | 0x0B,
 
         // Compilation block
         Error_MissingOutputAssignment = Error | CompileError | 0x01,
