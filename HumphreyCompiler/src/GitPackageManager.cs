d002aff6e4a9a98e4a04e06d888e2673e6259db3
Savoury SnaX <savoury.snax@googlemail.com>
2021-04-25 15:54:26 +0100

    Add libgit2sharp Add barebones repo test Add support for git repos as additional packages for libs/etc (not yet hooked into program.cs)

---
+++ HumphreyCompiler/src/GitPackageManager.cs
@@ -0,0 +1,81 @@
+
+using System.Collections.Generic;
+using LibGit2Sharp;
+
+namespace Humphrey
+{
+    public class GitPackageManager : IPackageManager
+    {
+        protected class GitPackageEntry : IPackageEntry, IPackageLevel
+        {
+            string _contents;
+
+            public GitPackageEntry(Blob gitBlob)
+            {
+                _contents = gitBlob.GetContentText();
+            }
+
+            public string Contents => _contents;
+
+            public IPackageLevel FetchEntry(string name)
+            {
+                return null;
+            }
+        }
+        protected class GitPackageLevel : IPackageLevel
+        {
+            private Tree _tree;
+            private Dictionary<string, IPackageLevel> _contents;
+            public GitPackageLevel(Tree tree)
+            {
+                _tree = tree;
+                _contents = new Dictionary<string, IPackageLevel>();
+            }
+            public IPackageLevel FetchEntry(string name)
+            {
+                if (_contents.TryGetValue(name, out var result))
+                {
+                    return result;
+                }
+                // perhaps not cached yet so scan tree
+                foreach(var e in _tree)
+                {
+                    var matchName = e.Name;
+                    if (e.Name.EndsWith(".humphrey"))
+                        matchName = e.Name.Substring(0, e.Name.LastIndexOf(".humphrey"));
+                    if (matchName==name)
+                    {
+                        if (e.TargetType == TreeEntryTargetType.Tree)
+                        {
+                            var level = new GitPackageLevel(e.Target.Peel<Tree>());
+                            _contents.Add(name, level);
+                            return level;
+                        }
+                        else if (e.TargetType == TreeEntryTargetType.Blob)
+                        {
+                            var entry = new GitPackageEntry(e.Target.Peel<Blob>());
+                            _contents.Add(name, entry);
+                            return entry;
+                        }
+                        else
+                        {
+                            throw new System.NotImplementedException($"Not a valid humprey package");
+                        }
+                    }
+                }
+                return null;
+            }
+        }
+
+        private GitPackageLevel _root;
+
+        public GitPackageManager(string repository, string revision)
+        {
+            var repo = new Repository(repository);
+            repo.RevParse(revision, out var reference, out var obj);
+            _root = new GitPackageLevel(obj.Peel<Tree>());
+        }
+
+        public IPackageLevel FetchRoot => _root;
+    }
+}
