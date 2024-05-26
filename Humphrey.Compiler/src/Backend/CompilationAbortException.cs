50130d92358b0865c406bd7fab0bb1d29239cce7
Savoury SnaX <savoury.snax@googlemail.com>
2024-05-26 17:34:23 +0100

    Bump to net8

--- Humphrey.Compiler/src/Backend/CompilationAbortException.cs
+++ Humphrey.Compiler/src/Backend/CompilationAbortException.cs
@@ -5,9 +5,5 @@ namespace Humphrey.Backend
     {
         public CompilationAbortException() { }
         public CompilationAbortException(string message) : base(message) { }
-        public CompilationAbortException(string message, System.Exception inner) : base(message, inner) { }
-        protected CompilationAbortException(
-            System.Runtime.Serialization.SerializationInfo info,
-            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
 }
